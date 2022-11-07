583bc71a48ecba1c6e8321c19ac9d8b31021998d
FizzChan <fizz.from.outerspace@gmail.com>
2022-11-07 12:33:27 +0900

    SQLITE ADVANCING

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Data.SQLite;
 
 namespace VetDI {
     /// <summary>
@@ -22,6 +23,48 @@ namespace VetDI {
         //コンストラクタ
         public MainWindow () {
             InitializeComponent ();
+            var sqlConnectionSb = new SQLiteConnectionStringBuilder{DataSource = "vdi.db"};
+            using(var cn = new SQLiteConnection(sqlConnectionSb.ToString())){
+                cn.Open();
+                using(var cmd = new SQLiteCommand(cn)){
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS vdi(" + 
+                        "serial INTEGER NOT NULL PRIMARY KEY," +
+                        "issuing INTEGER NOT NULL," +
+                        "name TEXT NOT NULL," +
+                        "address TEXT NOT NULL," +
+                        "phone TEXT NOT NULL," +
+                        "cause TEXT NOT NULL," +
+                        "dragname TEXT NOT NULL," +
+                        "dragqty TEXT NOT NULL," +
+                        "species TEXT NOT NULL," +
+                        "headage TEXT NOT NULL," +
+                        "age TEXT NOT NULL," +
+                        "aspect TEXT NOT NULL," +
+                        "regimen TEXT NOT NULL," +
+                        "dosage TEXT NOT NULL," +
+                        "taking TEXT NOT NULL," +
+                        "holidays TEXT NOT NULL," +
+                        "other TEXT NOT NULL)";
+                    cmd.ExecuteNonQuery();
+                    cmd.Insertvdi(20130011, 
+                        111111, 
+                        "(有)大窪ファーム", 
+                        "志布志市松山町新橋３８２５", 
+                        "099-487-2690",
+                        "AR・豚丹毒の予防",
+                        "AR-BP豚丹毒混合不活化ワクチン",
+                        "50ml*3",
+                        "母豚",
+                        "１５頭",
+                        "１～３歳齢",
+                        "無",
+                        "筋注",
+                        "5ml/頭、２回投与",
+                        "分娩前１ヶ月、２ヶ月の２回接種",
+                        "無",
+                        "無");
+                }
+            }
             dataGrid1.ItemsSource = new ObservableCollection<Instructions>{
                 new Instructions {
                     Serial=20130011,
@@ -70,6 +113,14 @@ namespace VetDI {
 
         }
 
+    }
+    public static class SQLiteExtention{
+        public static int Insertvdi(this SQLiteCommand cmd, int serial, int issuing, string name, string address, string phone, string cause, string dragname,
+            string dragqty, string species, string headage, string age, string aspect, string regimen, string dosage, string taking, string holidays, string other){
+            cmd.CommandText = "INSERT INTO vdi(serial, issuing, name, address, phone, cause, dragname, dragqty, species, headage, age, aspect, regimen, dosage, taking, holidays, other) " +
+                $"VALUES({serial}, {issuing}, '{name}', '{address}', '{phone}', '{cause}', '{dragname}', '{dragqty}', '{species}', '{headage}', '{age}', '{aspect}', '{regimen}', '{dosage}', '{taking}', '{holidays}', '{other}')";
+            return cmd.ExecuteNonQuery();
+        }
     }
 
 }
