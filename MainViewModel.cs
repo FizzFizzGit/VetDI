25c58a2594cf64e52f6311d15f95360520cb2352
FizzChan <fizz.from.outerspace@gmail.com>
2025-06-12 02:39:57 +0900

    editorconfig

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -1,54 +1,42 @@
 using System.ComponentModel;
 
-namespace VetDI
-{
-    public class MainViewModel : INotifyPropertyChanged
-    {
+namespace VetDI {
+    public class MainViewModel : INotifyPropertyChanged {
         private const string KeywordSearchDefaultText = "検索";
         private string _keyword = KeywordSearchDefaultText; // 初期値をデフォルトテキストに設定
-        public string Keyword
-        {
+        public string Keyword {
             get => _keyword;
-            set
-            {
-                if (_keyword != value)
-                {
+            set {
+                if (_keyword != value) {
                     _keyword = value;
                     OnPropertyChanged(nameof(Keyword));
                 }
             }
         }
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged(string name)
-        {
+        protected void OnPropertyChanged(string name) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
 
-        public void OnWindowClosing()
-        {
+        public void OnWindowClosing() {
             // データ保存やクリーンアップ処理
         }
 
-        public void OnSubmit()
-        {
+        public void OnSubmit() {
             // 検索処理の実装
             if (string.IsNullOrEmpty(Keyword)) { return; } // キーワードが空の場合は何もしない
             else if (Keyword == KeywordSearchDefaultText) { return; } // デフォルトテキストの場合も何もしない
             else { Keyword = KeywordSearchDefaultText; } // 検索後にデフォルトテキストに戻す
         }
 
-        public void ClearKeywordIfActivate()
-        {
+        public void ClearKeywordIfActivate() {
             if (Keyword == KeywordSearchDefaultText)
                 Keyword = "";
         }
 
-        public void SetKeywordIfDeactivate()
-        {
+        public void SetKeywordIfDeactivate() {
             if (Keyword == "")
                 Keyword = KeywordSearchDefaultText;
         }
-
     }
-    
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,44 +12,42 @@ namespace VetDI
         private const string ABOUT_DESCRIPTION = "about\n\n";
         private const string SEARCH_DEFAULT_TEXT = "検索";
         //コンストラクタ
-        public MainWindow () {
+        public MainWindow() {
             InitializeComponent();
             DataContext = new MainViewModel();
             Loaded += MainWindow_Loaded;
             Closing += MainWindow_Closing;
-
         }
 
-        private void MainWindow_Closing (object sender, System.ComponentModel.CancelEventArgs e) {
-            if (DataContext is MainViewModel vm)
-            {
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            if (DataContext is MainViewModel vm) {
                 vm.OnWindowClosing();
             }
             // 必要ならe.Cancel = true; でキャンセルも可能
         }
 
-        private void MainWindow_Loaded (object sender, RoutedEventArgs e) { }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e) { }
 
-        private void Button_About_Click (object sender, RoutedEventArgs e) {
+        private void Button_About_Click(object sender, RoutedEventArgs e) {
             MessageBox.Show(messageBoxText: ABOUT_DESCRIPTION, caption: "About");
         }
 
         private void Button_Submit_Click(object sender, RoutedEventArgs e) {
-            if (DataContext is MainViewModel vm){
+            if (DataContext is MainViewModel vm) {
                 vm.OnSubmit();
             }
         }
 
-        private void OnGotFocusHandler (object sender, RoutedEventArgs e) {
-            if (DataContext is MainViewModel vm){
+        private void OnGotFocusHandler(object sender, RoutedEventArgs e) {
+            if (DataContext is MainViewModel vm) {
                 vm.ClearKeywordIfActivate();
             }
         }
 
-        private void OnLostFocusHandler (object sender, RoutedEventArgs e) {
-            if (DataContext is MainViewModel vm){
+        private void OnLostFocusHandler(object sender, RoutedEventArgs e) {
+            if (DataContext is MainViewModel vm) {
                 vm.SetKeywordIfDeactivate();
             }
         }
-    }    
+    }
 }
